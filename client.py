c10e29480aad0bca83e423bfb3c88ea4dcc636b4
Govind S Warrier <govindswarrier@gmail.com>
2023-04-29 13:43:34 +0530

    5

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import json
-import urequests
+import requests
 
 # Send the two variables to the server
-url = 'http://localhost:8000'
+url = 'http://localhost:8000'  # Change this to the address of your server
 data = {'var1': 1.0, 'var2': 2.0}
-response = urequests.post(url, json=data)
+response = requests.post(url, json=data)
 
 # Parse the response JSON
 result = json.loads(response.text)
 result1 = result['result1']
 result2 = result['result2']
-print(f"Received results: {result1}, {result2}")
+print("Received results: {result1}, {result2}")
