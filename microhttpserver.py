1f27be581c438c68d5b72afe27831e4b64897b52
Govind S Warrier <govindswarrier@gmail.com>
2023-05-01 20:45:01 +0530

    http tcp server update

--- microhttpserver.py
+++ microhttpserver.py
@@ -1,27 +1,38 @@
-import network
-import machine
-import socket
+import http.server
+import socketserver
 
-# Set up Wi-Fi connection
-ssid = "AndroidAP"
-password = "gsw@1234"
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
-while not wlan.isconnected():
-    pass
+PORT = 8000
 
-# Define server function
-def http_server():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', 80))
-    s.listen(5)
-    while True:
-        conn, addr = s.accept()
-        request = conn.recv(1024)
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello World!"
-        conn.send(response.encode())
-        conn.close()
+Handler = http.server.SimpleHTTPRequestHandler
 
-# Run the server function
-http_server()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+# import network
+# import machine
+# import socket
+
+# # Set up Wi-Fi connection
+# ssid = "AndroidAP"
+# password = "gsw@1234"
+# wlan = network.WLAN(network.STA_IF)
+# wlan.active(True)
+# wlan.connect(ssid, password)
+# while not wlan.isconnected():
+#     pass
+
+# # Define server function
+# def http_server():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.bind(('0.0.0.0', 80))
+#     s.listen(5)
+#     while True:
+#         conn, addr = s.accept()
+#         request = conn.recv(1024)
+#         response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello World!"
+#         conn.send(response.encode())
+#         conn.close()
+
+# # Run the server function
+# http_server()
