a7ffbaaa0297ae2b0c768514fb04e66d7d3a3ffa
Govind S Warrier <govindswarrier@gmail.com>
2023-05-02 20:48:09 +0530

    new update

--- server2.py
+++ server2.py
@@ -1,5 +1,5 @@
 import json
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class Server2Handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -7,12 +7,12 @@ class Server2Handler(BaseHTTPRequestHandler):
         json_data = self.rfile.read(content_length)
         data = json.loads(json_data)
         message = data.get('message')
-        print('Received message from Server 1: {}'.format(message))
+        print 'Received message from Server 1:', message
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('Hello from Server 2'.encode('utf-8'))
+        self.wfile.write('Hello from Server 2')
 
 server2 = HTTPServer(('localhost', 8081), Server2Handler)
-print('Starting Server 2...')
+print 'Starting Server 2...'
 server2.serve_forever()
