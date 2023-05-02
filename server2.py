aa6846c99547a10e9f846644378b5a57694ab2a3
Govind S Warrier <govindswarrier@gmail.com>
2023-05-03 01:05:28 +0530

    ss

--- server2.py
+++ server2.py
@@ -10,8 +10,8 @@ class Server2Handler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         json_data = self.rfile.read(content_length)
         print type(json_data)
-        data = json.loads(json_data)
-        message = data.get('message')
+        # data = json.loads(json_data)
+        message = json_data.get('message')
         print 'Received message from Server 1:', message
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
