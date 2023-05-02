0b26c9132e2252883ed2c1abb95a0a4d40505c1f
Govind S Warrier <govindswarrier@gmail.com>
2023-05-03 01:49:49 +0530

    55

--- server2.py
+++ server2.py
@@ -1,6 +1,6 @@
 import json
-from SocketServer import ThreadingMixIn
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
@@ -9,16 +9,16 @@ class Server2Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         json_data = self.rfile.read(content_length)
-        print type(json_data)
-        data = json.loads(json_data)
+        print(type(json_data))
+        data = json.loads(json_data.decode('utf-8'))
         message = data.get('value')
 
-        print 'Received message from Server 1:', message
+        print('Received message from Server 1:', message)
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('Hello from Server 2')
+        self.wfile.write('Hello from Server 2'.encode('utf-8'))
 
 server2 = ThreadedHTTPServer(('', 8081), Server2Handler)
-print 'Starting Server 2...'
+print('Starting Server 2...')
 server2.serve_forever()
