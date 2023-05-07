8a769ae6d62e4fdc02003037c23474093769ae9e
Govind S Warrier <govindswarrier@gmail.com>
2023-05-07 21:04:54 +0530

    change

--- server2.py
+++ server2.py
@@ -1,34 +1,22 @@
 import json
-from SocketServer import ThreadingMixIn
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
 
 class Server2Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         json_data = self.rfile.read(content_length)
         data = json.loads(json_data)
-        message = data.get('message')
-        id = data.get('id')
-        print 'Received message from', id, ':', message
-
-        # Forward the message to the esp32
-        # Modify the code below to fit the specific requirements of your esp32 communication protocol
-        import socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('10.0.2.118', 8080))
-        s.sendall(message)
-        s.close()
-
-        response = {'message': 'Hello from Server 2'}
+        value1 = data.get('value1')
+        value2 = data.get('value2')
+        value3 = data.get('value3')
+        print 'Received values from ESP32:', value1, value2, value3
+        response = {'value1': value1, 'value2': value2, 'value3': value3}
         response_json = json.dumps(response)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(response_json)
 
-server2 = ThreadedHTTPServer(('', 8081), Server2Handler)
+server2 = HTTPServer(('', 8081), Server2Handler)
 print 'Starting Server 2...'
 server2.serve_forever()
