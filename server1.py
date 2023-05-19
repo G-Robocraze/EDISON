c5d4ddae5a2778a4cbf6403c63ec7af2baf622dc
Govind S Warrier <govindswarrier@gmail.com>
2023-05-19 22:43:12 +0530

    server update

--- server1.py
+++ server1.py
@@ -1,5 +1,8 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import random
+import time
+import httplib
 import json
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -13,11 +16,61 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write('Data received successfully')
 
-def run():
+def run_server():
     server_address = ('', 5000)
     httpd = HTTPServer(server_address, RequestHandler)
     print('Starting server on port 5000...')
     httpd.serve_forever()
 
+def send_data():
+    while True:
+        # Generate random data
+        voltage1 = random.randint(220, 240)
+        current1 = random.randint(1, 10)
+        energy1 = voltage1 * current1
+        voltage2 = random.randint(220, 240)
+        current2 = random.randint(1, 10)
+        energy2 = voltage2 * current2
+        voltage3 = random.randint(220, 240)
+        current3 = random.randint(1, 10)
+        energy3 = voltage3 * current3
+
+        data = {
+            'voltage1': voltage1,
+            'current1': current1,
+            'energy1': energy1,
+            'voltage2': voltage2,
+            'current2': current2,
+            'energy2': energy2,
+            'voltage3': voltage3,
+            'current3': current3,
+            'energy3': energy3
+        }
+
+        # Convert data to JSON format
+        json_data = json.dumps(data)
+
+        # Send the data to the server
+        conn = httplib.HTTPConnection('localhost', 5000)
+        headers = {'Content-type': 'application/json'}
+        conn.request('POST', '/endpoint', json_data, headers)
+        response = conn.getresponse()
+
+        if response.status == 200:
+            print('Data sent successfully')
+        else:
+            print('Failed to send data')
+
+        conn.close()
+
+        time.sleep(5)  # Delay for 5 seconds before sending the next data
+
 if __name__ == '__main__':
-    run()
+    # Start the server in a separate thread
+    import threading
+    server_thread = threading.Thread(target=run_server)
+    server_thread.daemon = True
+    server_thread.start()
+
+    # Start sending data
+    send_data()
