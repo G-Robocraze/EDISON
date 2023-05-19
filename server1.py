48f2b661632ae045c4daac7494659799f3f2982f
Govind S Warrier <govindswarrier@gmail.com>
2023-05-19 22:10:17 +0530

    updated server1

--- server1.py
+++ server1.py
@@ -1,12 +1,23 @@
-from flask import Flask, request
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import json
-app = Flask(__name__)
 
-@app.route('/endpoint', methods=['POST'])
-def handle_data():
-    data = request.get_json(force=True)
-    print(data)  # Process the received JSON data as desired
-    return 'Data received successfully'
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        data = self.rfile.read(content_length)
+        json_data = json.loads(data)
+        print(json_data)  # Process the received JSON data as desired
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write('Data received successfully')
+
+def run():
+    server_address = ('', 5000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print('Starting server on port 5000...')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    run()
