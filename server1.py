e7b9647d5cec41f534cd02d7aa0667bc79dddc68
Govind S Warrier <govindswarrier@gmail.com>
2023-05-22 13:42:26 +0530

    delay

--- server1.py
+++ server1.py
@@ -104,7 +104,7 @@ def send_data():
 
         conn.close()
 
-        time.sleep(2)  # Delay for 5 seconds before sending the next data
+        time.sleep(1)  # Delay for 5 seconds before sending the next data
 
 
 if __name__ == '__main__':
